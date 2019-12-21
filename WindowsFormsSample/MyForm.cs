7297e9f972f274bf628283f924f60074eee2d221
Burcak Kam <budemirc@microsoft.com>
2019-12-21 15:33:35 +0100

    Initialize the application

---
+++ WindowsFormsSample/MyForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleCSharp
+{
+    public class MyForm : Form
+    {
+        public MyForm() : base()
+        {
+            this.Text = "My Form";
+        }
+    }
+}
