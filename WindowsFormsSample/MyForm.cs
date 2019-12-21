0998c250d80ff2eef4b5bee064bfdf4e6ecd90dc
Burcak Kam <budemirc@microsoft.com>
2019-12-21 15:48:53 +0100

    Added Button, Textbox and Label

--- WindowsFormsSample/MyForm.cs
+++ WindowsFormsSample/MyForm.cs
@@ -9,9 +9,33 @@ namespace SampleCSharp
 {
     public class MyForm : Form
     {
+        private TextBox messageTextBox;
+        private Label messageLabel;
+        private Button showMessageButton;
+
         public MyForm() : base()
         {
             this.Text = "My Form";
+
+            messageTextBox = new TextBox();
+            messageTextBox.Left = 25;
+            messageTextBox.Top = 25;
+            messageTextBox.Width = 200;
+            this.Controls.Add(messageTextBox);
+
+            showMessageButton = new Button();
+            showMessageButton.Left = 25;
+            showMessageButton.Top = 75;
+            showMessageButton.Width = 200;
+            showMessageButton.Text = "Show Message";
+            this.Controls.Add(showMessageButton);
+
+            messageLabel = new Label();
+            messageLabel.Left = 25;
+            messageLabel.Top = 125;
+            messageLabel.Width = 200;
+            messageLabel.Text = "[Label]";
+            this.Controls.Add(messageLabel);
         }
     }
 }
